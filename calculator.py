53acb0a8e900265856c454463daefdea60779694
Sharoy Veduchi <61427449+doodayev@users.noreply.github.com>
2021-10-14 02:47:40 -0700

    Added the calculator program

---
+++ calculator.py
@@ -0,0 +1,29 @@
+import re
+
+def calculator(number1, number2, operator):
+    number1=float(number1)
+    number2=float(number2)
+    if(operator=="+"):
+        print(float(number1+number2))
+    if(operator=="-"):
+        print(float(number1-number2))
+    if(operator=="*"):
+        print(float(number1*number2))
+    if(operator=="/"):
+        print(float(number1/number2))
+    if(operator=="**"):
+        print(float(number1**number2))
+    
+def parse_input():
+    numba1=""
+    numba2=""
+    number1Done="False"
+    ParseThis=input("Enter equation: ")
+    Bruh=re.findall(r'[0-9\.]+|[^0-9\.]+', ParseThis)
+    #Bruh=re.split(r'(\D)', ParseThis)
+    calculator(Bruh[0], Bruh[2], Bruh[1])
+    
+    
+
+#parse_input()
+
